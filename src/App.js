20e8a985828159413298fd1a9b226df59f4ef52f
Alfiya6282823310 <knalfiya@gmail.com>
2024-07-27 23:49:31 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import Signin from './components/Signin';
+
 
 function App() {
   return (
     <div>
       <SignUp/>
+      <Signin/>
     </div>
   );
 }
