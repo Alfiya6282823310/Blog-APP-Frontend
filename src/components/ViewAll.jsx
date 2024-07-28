c9703e99f9ee19dbf6612aafc6f3e3407e3beb1f
Alfiya6282823310 <knalfiya@gmail.com>
2024-07-28 20:01:01 +0530

    viewall completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const ViewAll = () => {
+    const [data, setData] = useState([])
+    const token= sessionStorage.getItem("token")
+    const fetchData = () => {
+        axios.post("http://localhost:8081/viewall",{}, {
+            headers: { "token":token, "Content-Type": "application/json" }
+        }).then(
+            (response) => {
+                console.log(response.data)
+                setData(response.data)
+
+            }
+        ).catch(
+            (error) => {
+                console.log(error)
+            }
+        )
+    }
+    useEffect(()=>{fetchData()},[])
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            {
+                                data.map(
+                                    (value, index) => {
+                                        return <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                            <div class="card mb-3">
+                                                <div class="row g-0">
+                                                    <div class="col-md-4">
+                                                        <img src="https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=" class="img-fluid rounded-start" alt="..." />
+                                                    </div>
+                                                    <div class="col-md-8">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">{value.message}</h5>
+                                                
+                                                            <p class="card-text"><small class="text-body-secondary">posted date{value.postedDate}</small></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    }
+                                )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
