529b8417a1e1e419e187b7b1e3ceafdd06e0340b
Alfiya6282823310 <knalfiya@gmail.com>
2024-07-28 21:54:57 +0530

    project finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
 const Navbar = () => {
   const navigate=useNavigate()
   const logOut=()=>{
@@ -12,27 +11,35 @@ const Navbar = () => {
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Blog App</a>
+    <Link class="navbar-brand" to="#">Blog App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/create">create post</a>
+          <Link class="nav-link active" aria-current="page" to="/create">create post</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/viewall">viewall post</a>
+          <Link class="nav-link" to="/viewall">viewall post</Link>
         </li>
         
         <li class="nav-item">
-          <a class="nav-link" href='/viewmypost'>my post</a>
+          <Link class="nav-link" to='/viewmypost'>my post</Link>
         </li>
         <li class="nav-item">
           <button onClick={logOut} className="btn btn-success">Log out</button>
         </li>
       </ul>
       
+          <Link class="nav-link dropdown" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="https://tse1.mm.bing.net/th?id=OIP.mpXg7tyCFEecqgUsoW9eQwHaHk&pid=Api&P=0&h=220" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+            welcome
+          </Link>
+          
+          
+      
+  
     </div>
   </div>
 </nav>
